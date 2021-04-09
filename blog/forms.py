658fef5a60fd814467a3fd77c2c72550b1e8dd42
developerehis <developerehis@gmail.com>
2021-04-09 11:39:00 -0400

    files update

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={
         'class': 'form-control',
-        'placeholder' : 'Type your comment',
+        'placeholder' : 'Leave a comment',
         'id':'usercomment',
         'rows':'3',
     }))
